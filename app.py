53a45eb780cd2c39ff97023c27d86594c960206c
TARIQ AZIZ <rockersss250@gmail.com>
2025-03-13 19:31:07 +0530

    Add files via upload
    update ui

--- app.py
+++ app.py
@@ -77,36 +77,40 @@ def home_page():
 def predict_page():
     st.markdown("<h2 style='color: #0F0E47;'>📸 Upload Plant Leaf Image</h2>", unsafe_allow_html=True)
     test_image = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
+    
     if test_image is not None:
-        st.image(test_image, caption="Uploaded Image", use_container_width=True)
+        col1, col2 = st.columns([1, 1])  
+        with col1:
+            st.image(test_image, caption="Uploaded Image", use_container_width=True, output_format="auto", width=300)  
         save_path = os.path.join(os.getcwd(), "uploaded_image.jpg")
         with open(save_path, "wb") as f:
             f.write(test_image.getbuffer())
-        if st.button("🔍 Predict Disease"):
-            with st.spinner("Analyzing Image..."):
-                result_index = model_predict(save_path)
-                class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
-                              'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
-                              'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
-                              'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 
-                              'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
-                              'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
-                              'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 
-                              'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 
-                              'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 
-                              'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 
-                              'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 
-                              'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 
-                              'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
-                              'Tomato___healthy']
-                prediction = class_name[result_index]
-                st.success(f"✅ Prediction: {prediction}")
-                st.markdown(f"""
-                    <div style="text-align: center; padding: 20px; border-radius: 10px; background-color: #333333; color: #ffffff;">
-                        <h3>Prediction Successful!</h3>
-                        <p style="font-size: 1.2em;"><strong>{prediction}</strong></p>
-                    </div>
-                """, unsafe_allow_html=True)
+        with col2:
+            if st.button("🔍 Predict Disease"):
+                with st.spinner("Analyzing Image..."):
+                    result_index = model_predict(save_path)    
+                    class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+                                'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
+                                'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
+                                'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 
+                                'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
+                                'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+                                'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 
+                                'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 
+                                'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 
+                                'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 
+                                'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 
+                                'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 
+                                'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                                'Tomato___healthy']
+                    prediction = class_name[result_index]
+                    st.success(f"✅ Prediction: {prediction}")
+                    st.markdown(f"""
+                        <div style="text-align: center; padding: 20px; border-radius: 10px; background-color: #333333; color: #ffffff;">
+                            <h3>Prediction Successful!</h3>
+                            <p style="font-size: 1.2em;"><strong>{prediction}</strong></p>
+                        </div>
+                    """, unsafe_allow_html=True)
 
 def about_page():
     st.markdown("""
